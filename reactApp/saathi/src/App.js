cf12e054eedd9cb44dacebfe0e5aab191990b787
syedareehaquasar <reeha.saba@gmail.com>
2021-12-17 20:22:25 +0530

    initial frontend

---
+++ reactApp/saathi/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header.js'
+import './App.css';
+import Main from "./components/Main"
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactApp/saathi/src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Main() {
+    return (<div>
+        <div className="row">
+            <div className="col-md-3"><img src="https://fraternitysororitysvg.com/assets/images/products/Sexy-and-successful-svg.png" alt="hack4Women"/></div>
+            <div className="col-md-4">
+                <h1>Your Safe with Us!</h1>
+                <h4>
+                    This is a safe space! We are open to hear if you want to be heard!
+                    You can learn more and explore!
+                </h4>
+            </div>
+        </div>
+    </div>)
+}
+
+export default Main;
